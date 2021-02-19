854cb19ae9676017fa75d67a5ba34dbc5f0d4e46
Aaron Freeman <aaron@freeman.zone>
2021-02-19 12:10:17 -0600

    adding test output

--- src/test/kotlin/testery/testng/TestSomething.kt
+++ src/test/kotlin/testery/testng/TestSomething.kt
@@ -1,12 +1,14 @@
 package testery.testng
 
 import org.testng.Assert
+import org.testng.Reporter
 import org.testng.annotations.Test
 
 class TestSomething {
 
     @Test(groups = ["pass"])
     fun testSomeValue() {
+        Reporter.log("Output from test")
         Assert.assertEquals(Something.someValue,"foo")
     }
 }
